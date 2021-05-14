a0516ed10058a4b13935106592e41be1c174ee09
neolancelot2050 <auguslee2000@gmail.com>
2021-05-14 08:23:44 -0500

    Delete pruebadjango directory

--- pruebadjango/apphello/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApphelloConfig(AppConfig):
-    name = 'apphello'
--- pruebadjango/apphello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:nombre>", views.saludar, name="saludar"),
-    path("olivo", views.olivo, name="olivo"),
-
-]
